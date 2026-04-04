ca82f88277fdc09a87c4180a17564440f0cb8344
George Mavroeidis <gmav.graphics@gmail.com>
2026-04-04 05:24:52 -0400

    Core: Declare new core engine classes and structures

---
+++ bak3d/engine/Core/bak3d_object.h
@@ -0,0 +1,52 @@
+﻿/* ===========================================================================
+The MIT License (MIT)
+
+Copyright (c) 2022-2026 George Mavroeidis - GeoGraphics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+=========================================================================== */
+
+#pragma once
+
+#include <string>
+
+#include "glfw/src/internal.h"
+
+/*
+ * Base abstract object class for the entire engine.
+ * Contains simple metadata to help identify an instance across the entire project.
+ */
+class Bak3DObject
+{
+public:
+    Bak3DObject();
+    virtual ~Bak3DObject() = default;
+
+    const std::string& get_object_name() const { return m_object_name; }
+    void set_object_name(const std::string& name) { m_object_name = name; }
+
+    GLuint get_object_id() const { return m_object_id; }
+    void set_object_id(const GLuint id) { m_object_id = id; }
+
+    GLuint get_object_size() const { return m_object_size; }
+protected:
+    std::string m_object_name;
+    GLuint m_object_id = 0;
+    GLuint m_object_size = 0;
+};
